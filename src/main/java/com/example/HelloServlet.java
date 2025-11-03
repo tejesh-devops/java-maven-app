1e14b4e04fe14b90b619ecb8d63f341195319447
tejesh-devops <tejeshthej9100@gmail.com>
2025-11-03 18:39:29 +0530

    Update HelloServlet.java

--- src/main/java/com/example/HelloServlet.java
+++ src/main/java/com/example/HelloServlet.java
@@ -1,1 +1,16 @@
+package com.example;
 
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import java.io.*;
+
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<h2>Hello from Java Maven Web App!</h2>");
+    }
+}
