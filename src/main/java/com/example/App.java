7ec36774c0f073b340cbb7f2b77a765ae8fc221d
Tejesh <tejeshthej9100@gmail.com>
2025-11-03 15:05:24 +0530

    Initial commit - Java Maven app setup

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello from Jenkins + Maven Java App!");
+    }
+
+    public static String getMessage() {
+        return "Build Success!";
+    }
+}
+
